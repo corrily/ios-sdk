b466e211ce693f2dfa28987621fa0ac177deba59
Thành Trang <14819342+trangcongthanh@users.noreply.github.com>
2023-12-12 16:45:31 +0700

    add uikit example

---
+++ Example/CorrilyUIKit/CorrilyUIKit/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  CorrilyUIKit
+//
+//  Created by Thành Trang on 12/12/2023.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Example/CorrilyUIKit/CorrilyUIKit/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  CorrilyUIKit
+//
+//  Created by Thành Trang on 12/12/2023.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+  // Do any additional setup after loading the view.
+}
+
+
+@end
