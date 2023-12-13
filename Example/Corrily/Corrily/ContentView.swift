5f3ea258da12eeb9550b71cf2905b81f64d78282
Thành Trang <14819342+trangcongthanh@users.noreply.github.com>
2023-12-13 13:38:49 +0700

    change example to TabView

--- Example/Corrily/Corrily/ContentView.swift
+++ Example/Corrily/Corrily/ContentView.swift
@@ -9,39 +9,36 @@ import SwiftUI
 import CorrilySDK
 
 struct ContentView: View {
-  @State private var showPaywall = false
-  @State private var showPaywallCustomView = false
-  
   var body: some View {
-    
-        VStack(spacing: 24) {
-          Button(action: {
-            showPaywall = true
-          }, label: {
-            Text("Show Paywall")
-          })
-          .sheet(isPresented: $showPaywall) {
-            CorrilySDK.renderPaywall(action: {
-              showPaywall = false
-            })
-          }
-          Button(action: {
-            showPaywallCustomView = true
-          }, label: {
-            Text("Show Paywall with custom view")
-          })
-          .sheet(isPresented: $showPaywallCustomView) {
-            CorrilySDK.renderPaywall(customView: { factory in
-              CustomView(factory: factory, onSuccess: {
-                showPaywallCustomView = false
-              })
-            })
-          }
+    TabView {
+      DefaultTemplateView().tabItem {
+        VStack {
+          Image(systemName: "globe")
+          Text("Default Template")
         }
-        .padding()
+      }
+      CustomTemplateView().tabItem {
+        Image(systemName: "creditcard")
+        Text("Custom Template")
+      }
+    }
   }
 }
 
 #Preview {
   ContentView()
+}
+
+struct DefaultTemplateView: View {
+  var body: some View {
+    CorrilySDK.renderPaywall()
+  }
+}
+
+struct CustomTemplateView: View {
+  var body: some View {
+    CorrilySDK.renderPaywall(customView: { factory in
+      CustomView(factory: factory)
+    })
+  }
 }
